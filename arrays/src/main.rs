dbd16a22596bb4036ff0c06c1ad3615bc7992492
kkamara <kamaracomputers@gmail.com>
2026-02-06 15:09:41 +0000

    Add to project: arrays

--- arrays/src/main.rs
+++ arrays/src/main.rs
@@ -5,4 +5,13 @@ fn main() {
     let a: [i32; 5] = [1, 2, 3, 4, 5];
 
     println!("{:?}", a);
+
+    let _a = [3; 5];
+
+    // _a: The array named _a will contain 
+    // 5 elements that will all be set 
+    // to the value 3 initially. This is 
+    // the same as writing 
+    // let _a = [3, 3, 3, 3, 3]; but in 
+    // a more concise way.
 }

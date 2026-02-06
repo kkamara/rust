ee8b323f24465b3e1795d7b52d9d8652c94ec04a
kkamara <kamaracomputers@gmail.com>
2026-02-06 15:04:10 +0000

    Add project: arrays

---
+++ arrays/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let _months = ["January", "February", "March", "April", "May", "June", "July",
+              "August", "September", "October", "November", "December"];
+
+    let a: [i32; 5] = [1, 2, 3, 4, 5];
+
+    println!("{:?}", a);
+}

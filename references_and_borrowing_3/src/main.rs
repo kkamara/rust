23eb094deeba9d7fb787921f7692bdfb4e8299a2
kkamara <kamaracomputers@gmail.com>
2026-02-07 00:48:59 +0000

    Add project: references_and_borrowing_3

---
+++ references_and_borrowing_3/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    // Mutable references have one big
+    // restriction: If you have a
+    // mutable reference to a value,
+    // you can have no other references
+    // to that value.
+
+    let mut s = String::from("hello");
+
+    let r1 = &mut s;
+    let r2 = &mut s;
+
+    println!("{r1}, {r2}");
+}

88443500689b15394ea10b32d871f9d7160b7272
kkamara <kamaracomputers@gmail.com>
2026-02-07 00:56:18 +0000

    Add to project: references_and_borrowing_3

--- references_and_borrowing_3/src/main.rs
+++ references_and_borrowing_3/src/main.rs
@@ -5,10 +5,20 @@ fn main() {
     // you can have no other references
     // to that value.
 
+    let mut _s = String::from("hello");
+
+    // let r1 = &mut _s;
+    // let r2 = &mut _s; // Won't compile because of this.
+
+    // println!("{r1}, {r2}");
+
+    // The following is fine.
+
     let mut s = String::from("hello");
 
-    let r1 = &mut s;
-    let r2 = &mut s; // Won't compile because of this.
+    {
+        let _r1 = &mut s;
+    } // r1 goes out of scope here, so we can make a new reference with no problems.
 
-    println!("{r1}, {r2}");
+    let _r2 = &mut s;
 }

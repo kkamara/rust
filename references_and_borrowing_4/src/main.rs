f17d75e0dfde0c4ea231dbcd724b6838dd8c93f4
kkamara <kamaracomputers@gmail.com>
2026-02-07 01:03:46 +0000

    Add project: references_and_borrowing_4

---
+++ references_and_borrowing_4/src/main.rs
@@ -0,0 +1,21 @@
+fn main() {
+    /*
+    let mut s = String::from("hello");
+
+    let r1 = &s; // no problem
+    let r2 = &s; // no problem
+    let r3 = &mut s; // BIG PROBLEM
+
+    println!("{r1}, {r2}, and {r3}");
+    */
+
+    let mut s = String::from("hello");
+
+    let r1 = &s; // no problem
+    let r2 = &s; // no problem
+    println!("{r1} and {r2}");
+    // Variables r1 and r2 will not be used after this point.
+
+    let r3 = &mut s; // no problem
+    println!("{r3}");
+}

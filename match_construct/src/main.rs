be3ece5343ff002a220ce0d2ceaea2490137313b
kkamara <kamaracomputers@gmail.com>
2026-02-10 18:51:50 +0000

    Add project: match_construct

---
+++ match_construct/src/main.rs
@@ -0,0 +1,22 @@
+enum Coin {
+    Penny,
+    Nicket,
+    Dime,
+    Quarter,
+}
+
+fn main() {
+    println!(
+        "{}",
+        value_in_cents(Coin::Penny)
+    );
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nicket => 5,
+        Coin::Dime => 10,
+        Coin::Quarter => 25,
+    }
+}

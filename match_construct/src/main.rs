581dcb249492f3561a57a4b5ff6ae610821aae15
kkamara <kamaracomputers@gmail.com>
2026-02-10 19:37:17 +0000

    Add UsState enum to project: match_construct

--- match_construct/src/main.rs
+++ match_construct/src/main.rs
@@ -1,14 +1,72 @@
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+    Arizona,
+    Arkansas,
+    California,
+    Colorado,
+    Connecticut,
+    Delaware,
+    Florida,
+    Georgia,
+    Hawaii,
+    Idaho,
+    Illinois,
+    Indiana,
+    Iowa,
+    Kansas,
+    Kentucky,
+    Louisiana,
+    Maine,
+    Maryland,
+    Massachusetts,
+    Michigan,
+    Minnesota,
+    Mississippi,
+    Missouri,
+    Montana,
+    Nebraska,
+    Nevada,
+    NewHampshire,
+    NewJersey,
+    NewMexico,
+    NewYork,
+    NorthCarolina,
+    NorthDakota,
+    Ohio,
+    Oklahoma,
+    Oregon,
+    Pennsylvania,
+    RhodeIsland,
+    SouthCarolina,
+    SouthDakota,
+    Tennessee,
+    Texas,
+    Utah,
+    Vermont,
+    Virginia,
+    Washington,
+    WestVirginia,
+    Wisconsin,
+    Wyoming
+}
+
 enum Coin {
     Penny,
-    Nicket,
+    Nickel,
     Dime,
-    Quarter,
+    Quarter(UsState),
 }
 
 fn main() {
     println!(
         "{}",
-        value_in_cents(Coin::Penny)
+        value_in_cents(
+            Coin::Quarter(
+                UsState::Alaska
+            )
+        )
     );
 }
 
@@ -18,8 +76,14 @@ fn value_in_cents(coin: Coin) -> u8 {
             println!("Lucky penny!");
             1
         },
-        Coin::Nicket => 5,
+        Coin::Nickel => 5,
         Coin::Dime => 10,
-        Coin::Quarter => 25,
+        Coin::Quarter(state) => {
+            println!(
+                "State quarter from {:?}!",
+                state
+            );
+            25
+        },
     }
 }

a507ba29dc97a11f22b576e72a3ca7c73d8ca904
kkamara <kamaracomputers@gmail.com>
2026-02-12 16:35:13 +0000

    Add project: binary_crate

---
+++ binary_crate/src/main.rs
@@ -0,0 +1,10 @@
+// Example binary crate setup.
+// Contains a main.rs that serves
+// as the entry point for the binary.
+// Can also include a lib.rs if it
+// wants to expose a library API, but
+// it's not required.
+fn main() {
+    println!("Hello, world!");
+}
+// See project library_crate.

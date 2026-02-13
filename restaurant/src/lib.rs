bda5711c529c8bb2b1f7aff7ed4f566d46c197b7
kkamara <kamaracomputers@gmail.com>
2026-02-13 17:13:46 +0000

    Bringing Paths into Scope with the use Keyword

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -1,3 +1,7 @@
+// use std::{cmp::Ordering, io};
+use std::io::{self, Write}; // Import std::io and std::io::Write
+use std::collections::*;
+
 mod front_of_house {
     pub mod hosting {
         pub fn add_to_waitlist() {}
@@ -44,26 +48,36 @@ mod back_of_house {
 
 fn deliver_order() {}
 
-pub fn eat_at_restaurant() {
-    // Absolute path
-    // crate::front_of_house::hosting::add_to_waitlist();
+pub use crate::front_of_house::hosting;
 
-    // Relative path
-    // front_of_house::hosting::add_to_waitlist();
+mod customer {
+    // use super::hosting; // This does the same as the next line.
+    use crate::hosting;
+    use super::back_of_house;
 
-    // Order a breakfast in the summer with Rye road.
-    let mut meal = back_of_house::Breakfast::summer(
-        "Rye"
-    );
-    // Change our mind about what bread we'd like.
-    meal.toast = String::from("Wheat");
-    println!("I'd like {} toast please.", meal.toast);
+    pub fn eat_at_restaurant() {
+        // Absolute path
+        // crate::front_of_house::hosting::add_to_waitlist();
 
-    // The next line won't compile if we uncomment it;
-    // we're not allowed to see or modify the seasonal
-    // fruit that comes with the meal.
-    // meal.seasonal_fruit = String::from("blueberries");
+        // Relative path
+        // front_of_house::hosting::add_to_waitlist();
 
-    let order1 = back_of_house::Appetizer::Soup;
-    let order2 = back_of_house::Appetizer::Salad;
+        // Order a breakfast in the summer with Rye road.
+        let mut meal = back_of_house::Breakfast::summer(
+            "Rye"
+        );
+        // Change our mind about what bread we'd like.
+        meal.toast = String::from("Wheat");
+        println!("I'd like {} toast please.", meal.toast);
+
+        // The next line won't compile if we uncomment it;
+        // we're not allowed to see or modify the seasonal
+        // fruit that comes with the meal.
+        // meal.seasonal_fruit = String::from("blueberries");
+
+        let order1 = back_of_house::Appetizer::Soup;
+        let order2 = back_of_house::Appetizer::Salad;
+
+        hosting::add_to_waitlist();
+    }
 }

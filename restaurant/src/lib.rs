977dc8bceada34e499aaefa67f9a69e11f9bb858
kkamara <kamaracomputers@gmail.com>
2026-02-13 17:26:02 +0000

    Separating Modules into Different Files

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -2,49 +2,8 @@
 use std::io::{self, Write}; // Import std::io and std::io::Write
 use std::collections::*;
 
-mod front_of_house {
-    pub mod hosting {
-        pub fn add_to_waitlist() {}
-        
-        fn seat_at_table() {}
-    }
-
-    mod serving {
-        fn take_order() {}
-        
-        fn serve_order() {}
-
-        fn take_payment() {}
-    }
-}
-
-mod back_of_house {
-    fn fix_incorrect_order() {
-        cook_order();
-        super::deliver_order();
-    }
-
-    fn cook_order() {}
-
-    pub struct Breakfast {
-        pub toast: String,
-        seasonal_fruit: String,
-    }
-
-    impl Breakfast {
-        pub fn summer(toast: &str) -> Breakfast {
-            Breakfast {
-                toast: String::from(toast),
-                seasonal_fruit: String::from("peaches"),
-            }
-        }
-    }
-
-    pub enum Appetizer {
-        Soup,
-        Salad,
-    }
-}
+mod front_of_house;
+mod back_of_house;
 
 fn deliver_order() {}
 

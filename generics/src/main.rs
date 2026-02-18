512981d98368dec013016fdb6d56fcacb1bebf2d
kkamara <kamaracomputers@gmail.com>
2026-02-18 17:37:53 +0000

    Add comment to project generics

--- generics/src/main.rs
+++ generics/src/main.rs
@@ -7,4 +7,16 @@ fn main() {
     let both_integer = Point { x: 5, y: 10 };
     let both_float = Point { x: 1.0, y: 4.0 };
     let integer_and_float = Point { x: 5, y: 4.0 };
+
+    /*
+        Because Rust compiles generic code into
+        code that specifies the type in each
+        instance, we pay no runtime cost for
+        using generics. When the code runs, it
+        performs just as it would if we had
+        duplicated each definition by hand. The
+        process of monomorphization makes
+        Rust’s generics extremely efficient at
+        runtime.
+     */
 }
